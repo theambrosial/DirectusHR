97fa976ec5f6d861698ca7fd283195da2dd111cc
Harsh <harshkumarpathak01@gmail.com>
2020-04-15 18:36:01 +0530

    Theme Added with some error fixed

---
+++ directushr_web/react-ui/src/components/HorizontalLayout/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Row, Col } from "reactstrap";
+
+const Footer = () => {
+    return (
+        <React.Fragment>
+            <footer className="footer">
+                <div className="container-fluid">
+                    <Row>
+                        <Col md={6}>
+                            {new Date().getFullYear()} © Skote.
+                        </Col>
+                        <Col md={6}>
+                            <div className="text-sm-right d-none d-sm-block">
+                                Design & Develop by Themesbrand
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+            </footer>
+        </React.Fragment>
+    );
+};
+
+export default Footer;
--- directushr_web/react-ui/src/index.js
+++ directushr_web/react-ui/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-let myComponent =  document.getElementById('reactify-django-ui')
-if (myComponent !== null){
-    ReactDOM.render(<App />,myComponent);
-}
+import store from './store';
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
+serviceWorker.unregister();
