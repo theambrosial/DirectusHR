c284c2ae9c72568c61439fbf54ed114039192e9f
theambrosial <mail.ambrosial@gmail.com>
2020-05-11 15:06:35 +0530

    build for production

--- directushr_web/directushr_web/settings.py
+++ directushr_web/directushr_web/settings.py
@@ -30,10 +30,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'user_app',
-    # 'client_app',
-    # 'crm_app',
-    # 'candidate_app',
-    # 'recruiter_app',
+    'client_app',
+    'crm_app',
+    'candidate_app',
+    'recruiter_app',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ AUTH_USER_MODEL = 'user_app.SiteUser'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'react-ui','build')],     #change
+        'DIRS': [os.path.join(BASE_DIR, 'react-ui','build2')],     #change
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,16 +70,16 @@ WSGI_APPLICATION = 'directushr_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
- 'default': {
-     'ENGINE': 'django.db.backends.mysql',
-     'NAME': 'DHR_db',
-     'USER': 'postgres',
-     'PASSWORD': 'vikas123',
-     'HOST': '127.0.0.1',
-     'PORT': '5432',
- }
-}
+# DATABASES = {
+#  'default': {
+#      'ENGINE': 'django.db.backends.mysql',
+#      'NAME': 'DHR_db',
+#      'USER': 'postgres',
+#      'PASSWORD': 'vikas123',
+#      'HOST': '127.0.0.1',
+#      'PORT': '5432',
+#  }
+# }
 
 
 # Password validation
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-REACT_BUILD_DIR = os.path.join(BASE_DIR, 'react-ui', 'build')
+REACT_BUILD_DIR = os.path.join(BASE_DIR, 'react-ui', 'build2')
 
 
 # path = os.path.join(REACT_BUILD_DIR, "asset-manifest.json")
@@ -159,7 +159,7 @@ REACT_BUILD_DIR = os.path.join(BASE_DIR, 'react-ui', 'build')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, "static_local"),   #old
-    os.path.join(BASE_DIR, "react-ui", "build", "static"),    #change
+    os.path.join(BASE_DIR, "react-ui", "build2", "static"),    #change
 
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn", "static_root")
---
+++ directushr_web/react-ui/build2/precache-manifest.937664f04ac196daab81f074424e80ad.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6cd8128b610f2525d75462b455e2d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec2ba7635b50ca07d5d2",
+    "url": "/static/css/2.0d6255ba.chunk.css"
+  },
+  {
+    "revision": "88c6182275da6079bf5f",
+    "url": "/static/css/main.ea8281cc.chunk.css"
+  },
+  {
+    "revision": "ec2ba7635b50ca07d5d2",
+    "url": "/static/js/2.63f2e65c.chunk.js"
+  },
+  {
+    "revision": "2ed7abd64b910157ac18f2dd6dec5ed2",
+    "url": "/static/js/2.63f2e65c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c6182275da6079bf5f",
+    "url": "/static/js/main.5484242c.chunk.js"
+  },
+  {
+    "revision": "96d1375af4c48c2febff",
+    "url": "/static/js/runtime-main.17723031.js"
+  },
+  {
+    "revision": "a6ffbd81c1a0cf135aee8527add752d9",
+    "url": "/static/media/avatar-1.a6ffbd81.jpg"
+  },
+  {
+    "revision": "02aea0c2c148263b4c4de63a52284803",
+    "url": "/static/media/avatar-2.02aea0c2.jpg"
+  },
+  {
+    "revision": "41a180f34b2da436150ac0442c88b9f2",
+    "url": "/static/media/avatar-3.41a180f3.jpg"
+  },
+  {
+    "revision": "e0ab315a0e2e82eb9fec1387bae6600d",
+    "url": "/static/media/avatar-4.e0ab315a.jpg"
+  },
+  {
+    "revision": "3e35c39bc9b52c2ed8d9b599dadb17bc",
+    "url": "/static/media/avatar-5.3e35c39b.jpg"
+  },
+  {
+    "revision": "11d7dc6867e432ccb08c6a15e85d12e1",
+    "url": "/static/media/avatar-6.11d7dc68.jpg"
+  },
+  {
+    "revision": "9524b09f679d8774fdf9b1fffbb9ba7d",
+    "url": "/static/media/avatar-7.9524b09f.jpg"
+  },
+  {
+    "revision": "bc11bfd155133a7ddc7c342f440893e4",
+    "url": "/static/media/avatar-8.bc11bfd1.jpg"
+  },
+  {
+    "revision": "1ec26ee17fd6b5903e74b8a599166a22",
+    "url": "/static/media/boxicons.1ec26ee1.woff2"
+  },
+  {
+    "revision": "afbad0fa2cbbb785e034ae3e3bb328ff",
+    "url": "/static/media/boxicons.afbad0fa.eot"
+  },
+  {
+    "revision": "d80a04657ac29725e6985906800101e4",
+    "url": "/static/media/boxicons.d80a0465.svg"
+  },
+  {
+    "revision": "deea39cb9dc8b17f9d640014ca2f87af",
+    "url": "/static/media/boxicons.deea39cb.woff"
+  },
+  {
+    "revision": "f46ca69e63f9c5f080203c3f510b4110",
+    "url": "/static/media/boxicons.f46ca69e.ttf"
+  },
+  {
+    "revision": "0253a9a5e15b217b18ade085c5390ef5",
+    "url": "/static/media/dripicons-v2.0253a9a5.svg"
+  },
+  {
+    "revision": "11fc83ae11617015f2fcde2065fb34d3",
+    "url": "/static/media/dripicons-v2.11fc83ae.woff"
+  },
+  {
+    "revision": "7e12564e72400735ae5b671780f6a9f7",
+    "url": "/static/media/dripicons-v2.7e12564e.eot"
+  },
+  {
+    "revision": "cf09c981aeaa8736810133ab1148e4a3",
+    "url": "/static/media/dripicons-v2.cf09c981.ttf"
+  },
+  {
+    "revision": "c7ae99ce871c7f754a0271483f7d136f",
+    "url": "/static/media/error-img.c7ae99ce.png"
+  },
+  {
+    "revision": "3165b14bbf3b64fca65829ecde6b800d",
+    "url": "/static/media/fa-brands-400.3165b14b.eot"
+  },
+  {
+    "revision": "457cb96b6191ed105c4fe8463957f70d",
+    "url": "/static/media/fa-brands-400.457cb96b.woff"
+  },
+  {
+    "revision": "75af8363f4a38af4de07e056ffa068f9",
+    "url": "/static/media/fa-brands-400.75af8363.svg"
+  },
+  {
+    "revision": "f2e186cfab4787d4ef6f1bb192aa9a1b",
+    "url": "/static/media/fa-brands-400.f2e186cf.ttf"
+  },
+  {
+    "revision": "f861a57c52ef711cf807a3eec92c0e17",
+    "url": "/static/media/fa-brands-400.f861a57c.woff2"
+  },
+  {
+    "revision": "190b0a81aba4077923a625483784d680",
+    "url": "/static/media/fa-regular-400.190b0a81.svg"
+  },
+  {
+    "revision": "2cd8d991e82712f1a5c5de8ee869ca74",
+    "url": "/static/media/fa-regular-400.2cd8d991.ttf"
+  },
+  {
+    "revision": "7ab2cb050690f93994c46258d49af5b6",
+    "url": "/static/media/fa-regular-400.7ab2cb05.woff"
+  },
+  {
+    "revision": "a03df7ab0ffc96cabbfe3dc71c60baaa",
+    "url": "/static/media/fa-regular-400.a03df7ab.eot"
+  },
+  {
+    "revision": "bd52a727b5449dc3f8195b72c9c58341",
+    "url": "/static/media/fa-regular-400.bd52a727.woff2"
+  },
+  {
+    "revision": "2cd2be177470d5096992572176bbe76e",
+    "url": "/static/media/fa-solid-900.2cd2be17.woff2"
+  },
+  {
+    "revision": "5ee32f5c8598e1a63ddf1aad4ffe54f4",
+    "url": "/static/media/fa-solid-900.5ee32f5c.ttf"
+  },
+  {
+    "revision": "657a469469a2fc38a2901c68a3d56512",
+    "url": "/static/media/fa-solid-900.657a4694.woff"
+  },
+  {
+    "revision": "a547e21eceadf53602caf057be9ad9fd",
+    "url": "/static/media/fa-solid-900.a547e21e.eot"
+  },
+  {
+    "revision": "f4e2aa6a10b878a8db2ca3c5c916f30a",
+    "url": "/static/media/fa-solid-900.f4e2aa6a.svg"
+  },
+  {
+    "revision": "87fa32b714145a830f2cf83972a6c816",
+    "url": "/static/media/img-1.87fa32b7.jpg"
+  },
+  {
+    "revision": "a9b1b2a353be38935fb7029173bc36ca",
+    "url": "/static/media/img-1.a9b1b2a3.png"
+  },
+  {
+    "revision": "03f4858d99d26de2cda2e5cda3d25a4f",
+    "url": "/static/media/img-2.03f4858d.jpg"
+  },
+  {
+    "revision": "714cfaa84d2e3402537aa471e47593dc",
+    "url": "/static/media/img-2.714cfaa8.png"
+  },
+  {
+    "revision": "1cfde94e938bfeae442d531afff78bb0",
+    "url": "/static/media/img-3.1cfde94e.jpg"
+  },
+  {
+    "revision": "71cb7c45cc969e600c045cf3fce2d144",
+    "url": "/static/media/img-3.71cb7c45.png"
+  },
+  {
+    "revision": "13652570b25022df253dd8e694cdcd5a",
+    "url": "/static/media/img-4.13652570.png"
+  },
+  {
+    "revision": "825d4e1c18e4987ecbebefbe475e1ab2",
+    "url": "/static/media/img-4.825d4e1c.jpg"
+  },
+  {
+    "revision": "266125da4f0743aa52f8e9a4d1bbaa62",
+    "url": "/static/media/img-5.266125da.png"
+  },
+  {
+    "revision": "8abc0a08b5279ff0f7db06cfff39ee4b",
+    "url": "/static/media/img-5.8abc0a08.jpg"
+  },
+  {
+    "revision": "17b0e61cfdf2db0f10ce95a69e631f6d",
+    "url": "/static/media/img-6.17b0e61c.jpg"
+  },
+  {
+    "revision": "7821ebc744a5cd212ca59abdce32d27a",
+    "url": "/static/media/img-6.7821ebc7.png"
+  },
+  {
+    "revision": "4dbb8c9dfc18979ac9a77c7bd7baef7c",
+    "url": "/static/media/img-7.4dbb8c9d.jpg"
+  },
+  {
+    "revision": "5f29a91e4757ff169585789c4d180fd5",
+    "url": "/static/media/img-7.5f29a91e.png"
+  },
+  {
+    "revision": "5abe67816fe7e9fbd535697b4a3df1a5",
+    "url": "/static/media/img-8.5abe6781.png"
+  },
+  {
+    "revision": "a4629b6365cd7067c88dfee273ef5cb5",
+    "url": "/static/media/layout-1.a4629b63.jpg"
+  },
+  {
+    "revision": "5afd4b42bd0dcc7a6188c1e8c8a4b54a",
+    "url": "/static/media/layout-2.5afd4b42.jpg"
+  },
+  {
+    "revision": "06b3c2036bbdc7a43e88ecaedb073631",
+    "url": "/static/media/layout-3.06b3c203.jpg"
+  },
+  {
+    "revision": "c92032cc7e79e15ee82bbad2ce17c28f",
+    "url": "/static/media/logo-light.c92032cc.svg"
+  },
+  {
+    "revision": "c84b4ddb61fa3fcc80074749d95f9eb5",
+    "url": "/static/media/logo.c84b4ddb.jpg"
+  },
+  {
+    "revision": "f7686e584d8e57b8c640a110d3597246",
+    "url": "/static/media/logo.f7686e58.svg"
+  },
+  {
+    "revision": "2ab0d1198279de32fff039ae4949a806",
+    "url": "/static/media/maintenance.2ab0d119.png"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "/static/media/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "/static/media/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "/static/media/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "/static/media/materialdesignicons-webfont.a32fa1f2.eot"
+  },
+  {
+    "revision": "913183e44a90bcc2bf414c35a1ae6d11",
+    "url": "/static/media/megamenu-img.913183e4.png"
+  },
+  {
+    "revision": "ba4e037e1b9676c5215541748eaa1e42",
+    "url": "/static/media/profile-img.ba4e037e.png"
+  }
+]);
